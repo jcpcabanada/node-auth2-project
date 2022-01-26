342289fd51462c2ed7d3fdc7bd6733fee09fc8a8
jcpcabanada <jcpcabanada@gmail.com>
2022-01-25 20:34:57 -0800

    5th

--- api/auth/auth-middleware.js
+++ api/auth/auth-middleware.js
@@ -1,7 +1,22 @@
 const {JWT_SECRET} = require("../secrets"); // use this secret!
-const { find, findBy} = require('../users/users-model')
-const jtw = require('jsonwebtoken')
+const { findBy} = require('../users/users-model')
+const jwt = require('jsonwebtoken')
+
 const restricted = (req, res, next) => {
+    const token = req.headers.authorization
+    if(!token) {
+        next({status: 401, message: 'Token required'})
+    }
+    jwt.verify(token, JWT_SECRET, (err, decoded) => {
+        if(err) {
+            next({status: 401, message: 'Token invalid'})
+        }
+        else {
+            req.decoded = decoded
+            next()
+        }
+    })
+};
 
     /*
       If the user does not provide a token in the Authorization header:
@@ -18,25 +33,16 @@ const restricted = (req, res, next) => {
 
       Put the decoded token in the req object, to make life easier for middlewares downstream!
     */
-    const token = req.headers.authorization
-    if (!token) {
-        return next ({
-            status: 401, messsage: 'Token required'
-        })
-    }
-    jtw.verify(token, JWT_SECRET, (err, decodedToken) => {
-        if(err) {
-            next({
-                status: 401, messsage: 'Token invalid'
-            })
-        } else {
-            req.decodedToken = decodedToken
-            next()
-        }
-    })
-}
 
 const only = role_name => (req, res, next) => {
+    if (role_name === req.decoded.role_name) {
+        next()
+    } else {
+        next ({
+            status: 403, message: 'This is not for you'
+        })
+    }
+}
     /*
       If the user does not provide a token in the Authorization header with a role_name
       inside its payload matching the role_name passed to this function as its argument:
@@ -47,14 +53,6 @@ const only = role_name => (req, res, next) => {
 
       Pull the decoded token from the req object, to avoid verifying it again!
     */
-    if (role_name === req.decodedToken.role_name) {
-        next()
-    } else {
-        next ({
-            status: 400, message: 'This is not for you'
-        })
-    }
-}
 
 
 const checkUsernameExists = async (req, res, next) => {
