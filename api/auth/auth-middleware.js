d1b71a62980eafee0556386ebf7d4f4386920727
jcpcabanada <jcpcabanada@gmail.com>
2022-01-25 19:06:45 -0800

    3rd

--- api/auth/auth-middleware.js
+++ api/auth/auth-middleware.js
@@ -1,72 +1,88 @@
-const { JWT_SECRET } = require("../secrets"); // use this secret!
+const {JWT_SECRET} = require("../secrets"); // use this secret!
 
 const restricted = (req, res, next) => {
-  /*
-    If the user does not provide a token in the Authorization header:
-    status 401
-    {
-      "message": "Token required"
-    }
+    /*
+      If the user does not provide a token in the Authorization header:
+      status 401
+      {
+        "message": "Token required"
+      }
 
-    If the provided token does not verify:
-    status 401
-    {
-      "message": "Token invalid"
-    }
+      If the provided token does not verify:
+      status 401
+      {
+        "message": "Token invalid"
+      }
 
-    Put the decoded token in the req object, to make life easier for middlewares downstream!
-  */
+      Put the decoded token in the req object, to make life easier for middlewares downstream!
+    */
+    next()
 }
 
 const only = role_name => (req, res, next) => {
-  /*
-    If the user does not provide a token in the Authorization header with a role_name
-    inside its payload matching the role_name passed to this function as its argument:
-    status 403
-    {
-      "message": "This is not for you"
-    }
+    /*
+      If the user does not provide a token in the Authorization header with a role_name
+      inside its payload matching the role_name passed to this function as its argument:
+      status 403
+      {
+        "message": "This is not for you"
+      }
 
-    Pull the decoded token from the req object, to avoid verifying it again!
-  */
+      Pull the decoded token from the req object, to avoid verifying it again!
+    */
+    next()
 }
 
 
 const checkUsernameExists = (req, res, next) => {
-  /*
-    If the username in req.body does NOT exist in the database
-    status 401
-    {
-      "message": "Invalid credentials"
-    }
-  */
+    /*
+      If the username in req.body does NOT exist in the database
+      status 401
+      {
+        "message": "Invalid credentials"
+      }
+    */
+    next()
 }
 
 
 const validateRoleName = (req, res, next) => {
+
+  if (!req.body.role_name || !req.body.role_name.trim()) {
+    req.role_name = 'student'
+    next()
+  } else if (req.body.role_name.trim() === 'admin') {
+    next({status: 422, message: 'Role name can not be admin'})
+  } else if (req.body.role_name.trim().length > 32) {
+    next({status: 422, message: 'Role name can not be longer than 32 chars'})
+  } else {
+    next()
+  }
+
   /*
-    If the role_name in the body is valid, set req.role_name to be the trimmed string and proceed.
+      If the role_name in the body is valid, set req.role_name to be the trimmed string and proceed.
 
-    If role_name is missing from req.body, or if after trimming it is just an empty string,
-    set req.role_name to be 'student' and allow the request to proceed.
+      If role_name is missing from req.body, or if after trimming it is just an empty string,
+      set req.role_name to be 'student' and allow the request to proceed.
 
-    If role_name is 'admin' after trimming the string:
-    status 422
-    {
-      "message": "Role name can not be admin"
-    }
+      If role_name is 'admin' after trimming the string:
+      status 422
+      {
+        "message": "Role name can not be admin"
+      }
 
-    If role_name is over 32 characters after trimming the string:
-    status 422
-    {
-      "message": "Role name can not be longer than 32 chars"
-    }
-  */
+      If role_name is over 32 characters after trimming the string:
+      status 422
+      {
+        "message": "Role name can not be longer than 32 chars"
+      }
+    */
+
 }
 
 module.exports = {
-  restricted,
-  checkUsernameExists,
-  validateRoleName,
-  only,
+    restricted,
+    checkUsernameExists,
+    validateRoleName,
+    only,
 }
--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -3,6 +3,7 @@ const { checkUsernameExists, validateRoleName } = require('./auth-middleware');
 const { JWT_SECRET } = require("../secrets"); // use this secret!
 
 router.post("/register", validateRoleName, (req, res, next) => {
+
   /**
     [POST] /api/auth/register { "username": "anna", "password": "1234", "role_name": "angel" }
 
